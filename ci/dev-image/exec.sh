f07a2002ecb2626fb307a8617d1899c47d903dd5
cmoog <moogcharlie@gmail.com>
2020-04-07 16:31:33 +0000

    Formatting

--- ci/dev-image/exec.sh
+++ ci/dev-image/exec.sh
@@ -13,38 +13,31 @@ echo "--- Building $container_name"
 cd ../../ && docker build -t $container_name -f ./ci/dev-image/Dockerfile . > /dev/null
 
 set +e
-container_id=$(docker container inspect --format="{{.Id}}" $container_name 2>/dev/null)
+container_id=$(docker container inspect --format="{{.Id}}" $container_name 2> /dev/null)
 
 if [ $? -eq "0" ]; then
-    echo "--- Killing $container_name"
-    docker rm -f $container_name 2>/dev/null
+  echo "--- Killing $container_name"
+  docker rm -f $container_name 2> /dev/null
 fi
 set -e
 
 echo "--- Spawning $container_name"
 container_id=$(docker run \
-    -it \
-    --privileged \
-    --name $container_name \
-    "-v=$PWD:/code-server" \
-    "-w=/code-server" \
-    "-p=8080:8080" \
-    $([[ -t 0 ]] && echo -it || true) \
-    -d \
-    $container_name)
+  -it \
+  --privileged \
+  --name $container_name \
+  "-v=$PWD:/code-server" \
+  "-w=/code-server" \
+  "-p=8080:8080" \
+  $([[ -t 0 ]] && echo -it || true) \
+  -d \
+  $container_name)
 
 set +e
 
-echo "--- Executing: $@"
-
-CMD="$@"
+echo "--- Entering dev container"
 
 docker exec \
-    $([[ -t 0 ]] && echo -it || true) \
-    $container_id \
-    bash -c "$CMD"
-
-docker exec \
-    $([[ -t 0 ]] && echo -it || true) \
-    $container_id \
-    bash
+  $([[ -t 0 ]] && echo -it || true) \
+  $container_id \
+  bash
